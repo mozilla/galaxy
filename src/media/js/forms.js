537f83d59dea528a1deb70ebc9b03cfed3d50068
Chris Van <cvan@mozilla.com>
2013-12-19 21:30:11 -0800

    be even more clever with the form field focussing/blurring validation

--- src/media/js/forms.js
+++ src/media/js/forms.js
@@ -5,6 +5,7 @@ define('forms', ['z'], function(z) {
             $(form).filter(':not([novalidate])').find('button[type=submit]').attr('disabled', !form.checkValidity());
         }
     }
+
     z.body.on('change keyup paste', 'input, select, textarea', function(e) {
         checkValid(e.target.form);
     }).on('loaded decloak', function() {
@@ -12,6 +13,26 @@ define('forms', ['z'], function(z) {
             checkValid(this);
         });
         $('form[novalidate] button[type=submit]').removeAttr('disabled');
+    }).on('blur', 'input, select, textarea', function() {
+        // Add a class so we don't prematurely stylise `:invalid` fields.
+        var $this = $(this);
+        // If it's required, show :valid/:invalid styles -OR-
+        // if it's optional, show styles when there's some text in the value.
+        $this.toggleClass('focused', this.hasAttribute('required') || !!$this.val());
+        // if (!this.hasAttribute('required') && this.val())
+    }).on('focus', 'input[type=url]', function() {
+        var $this = $(this);
+        if (!$this.val()) {
+            $this.val('http://');
+        }
+    }).on('blur', 'input[type=url]', function() {
+        var $this = $(this);
+        if ($this.val() === 'http://') {
+            $this.val('');
+            if (!this.hasAttribute('required')) {
+                $this.removeClass('focused');
+            }
+        }
     });
 
     // Use this if you want to disable form inputs while the post/put happens.
--- src/media/js/views/submit.js
+++ src/media/js/views/submit.js
@@ -2,24 +2,20 @@ define('views/submit',
        ['dropzone', 'l10n', 'routes_api', 'storage', 'utils', 'z'],
        function(dropzone, l10n, routes_api, storage, utils, z) {
 
-    z.body.on('focus', '[pattern], [required], [maxlength]', function() {
-        // Add a class so we don't prematurely stylise `:invalid` fields.
-        var $this = $(this);
-        $this.addClass('focused');
-    }).on('focus', 'input[type=url]', function() {
-        var $this = $(this);
-        if (!$this.val()) {
-            $this.val('http://');
-        }
-    }).on('blur', 'input[type=url]', function() {
-        var $this = $(this);
-        if ($this.val() === 'http://') {
-            $this.val('');
-        }
-    }).on('blur keyup change paste', '[name=name]', function() {
+    z.body.on('blur change keyup paste', '[name=name]', function(e) {
         var $this = $(this);
         var $slug = $this.closest('form').find('[name=slug]');
-        $slug.val(utils.slugify($this.val())).addClass('focused');
+
+        $slug.val(utils.slugify($this.val()));
+
+        // If the name is valid, the slug is valid so let the user tab over it.
+        $slug.attr('tabIndex', this.checkValidity() ? '-1' : '');
+
+        // Upon blur of name, add the `focused` class so :valid/:invalid
+        // styles get set on slug.
+        if (e.type === 'blur' || e.type === 'focusout') {
+            $slug.addClass('focused');
+        }
     });
 
     return function(builder) {
