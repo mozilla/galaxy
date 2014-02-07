44b89af1d70e5175b732b11bb0b486d903f74b52
scottmeng <blackridermkz@gmail.com>
2014-02-07 15:13:31 -0800

    add data parameter to del function, add delete function to leaderboard page

--- src/media/js/views/leaderboard.js
+++ src/media/js/views/leaderboard.js
@@ -1,4 +1,34 @@
-define('views/leaderboard', ['l10n', 'log', 'utils', 'z'], function(l10n, log, utils, z) {
+define('views/leaderboard', 
+	['l10n', 'log', 'utils', 'z', 'urls', 'requests', 'notification'], 
+	function(l10n, log, utils, z, urls, requests, notification) {
+
+	var console = log("leaderboard");
+
+	function delboard(slug) {
+		console.log(slug);
+		console.log(urls.api.url('leaderboard.dev'));
+
+		requests.del(urls.api.url('leaderboard.dev'), {
+			slug: slug
+		}).done(function(data) {
+			notification.notification({message: gettext('Leaderboard deleted')});
+			return console.log(data);
+		}).fail(function(data) {
+			notification.notification({message: gettext('Failed to delete leaderboard')});
+			return console.error(data);
+		});
+	}
+
+	z.body.on('click', '.board-del', function(e) {
+		var $board = $(this).closest('[data-board-slug]');
+		var boardName = $board.find(".board-link").text();
+		var boardSlug = $board.data("boardSlug");
+		console.log(boardSlug);
+		var alert = gettext('Do you want to remove {boardName}', {boardName: boardName});
+		if (window.confirm(alert)) {
+			delboard(boardSlug);
+		}
+	});
 
     return function(builder, args) {
     	var slug = args[0];
