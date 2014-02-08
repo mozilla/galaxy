608afe58ae1eecf6417471eb4263b1bd8647e671
scottmeng <blackridermkz@gmail.com>
2014-02-08 10:33:17 -0800

    fixing mistakes, reloading page only if leaderboard is successfully created, adding auto-slugify ability

--- src/media/js/views/leaderboard.js
+++ src/media/js/views/leaderboard.js
@@ -20,12 +20,12 @@ define('views/leaderboard',
             slug: slug
         }).done(function(data) {
             notification.notification({message: gettext('Leaderboard created')});
+            window.location.reload(true);
             return console.log(data);
         }).fail(function(data) {
             notification.notification({message: gettext('Failed to create leaderboard')});
             return console.error(data);
         }); 
-        window.location.reload(true);
     }
 
     z.body.on('click', '.board-del', function(e) {
@@ -40,13 +40,27 @@ define('views/leaderboard',
             $board.remove();
             delBoard(gameSlug, boardSlug);
         }
+    }).on('blur change keyup paste', 'input[name=name]', function(e) {
+        // NOTE: We're using `keyup` instead of `keypress` to detect when
+        // the user tabs within this field.
+        var $this = $(this);
+        var $slug = $this.closest('form').find('[name=slug]');
+
+        $slug.val(utils.slugify($this.val()));
+
+        // If the name is valid, the slug is valid so let the user tab over it.
+        $slug.attr('tabIndex', this.checkValidity() ? '-1' : '');
+
+        // Upon presence/absence of name, toggle the `focused` class so
+        // :valid/:invalid styles get set on slug.
+        $slug.toggleClass('focused', !!$this.val());
     }).on('click', '.board-create', function(e) {
         e.preventDefault();
         var $this = $(this);
         var $form = $this.closest('[data-game-slug]');
         var game = $form.data('gameSlug');
-        var boardName = $form.find('.name').val();
-        var boardSlug = $form.find('.slug').val();
+        var boardName = $form.find('[name=name]').val();
+        var boardSlug = $form.find('[name=slug]').val();
         console.log(gettext("game: {game}; boardName: {boardName}; boardSlug: {boardSlug}", {
             game: game,
             boardName: boardName,
