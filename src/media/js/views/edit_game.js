1170585086d976eb3f89c2450b7e1df47b06e480
Birkan Uzun <birkanuzun27@gmail.com>
2014-02-08 12:48:13 -0800

    Add game editing UI/macro

--- src/media/js/views/edit_game.js
+++ src/media/js/views/edit_game.js
@@ -1,17 +1,14 @@
-define('views/edit_game', ['l10n', 'utils'], function(l10n, utils) {
-
+define('views/edit_game', ['l10n', 'utils', 'log', 'z'], function(l10n, utils, log, z) {
     var gettext = l10n.gettext;
-
+    
     return function(builder, args) {
     	var slug = args[0];
         builder.start('game/edit.html', {slug: slug});
-
+        
         builder.z('type', 'leaf game');
-        builder.z('title', gettext('Edit Game'));
 
         builder.onload('game-data', function(game) {
-        	var title = 'Edit' + utils.translate(game.name);
-            builder.z('title', title);
+            builder.z('title', utils.translate(game.name));
         });
     };
 });
