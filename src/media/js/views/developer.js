487341265097d715e4dc3ed39547599c239d25b7
emoore24 <evan.moore24@gmail.com>
2014-02-09 10:03:42 -0800

    developer profile page
    + define route for developer profile page
    + add macro for developer image
    + add initial developer view and template

---
+++ src/media/js/views/developer.js
@@ -0,0 +1,10 @@
+define('views/developer', [], function() {
+    return function(builder, args) {
+        var dev_slug = args[0];
+        builder.start('developers/profile.html', {dev_slug: dev_slug}).done(function() {
+        });
+
+        builder.z('type', 'leaf profile');
+        builder.z('title', gettext('User Profile'));
+    };
+});
