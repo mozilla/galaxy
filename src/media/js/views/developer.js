ee8655079239e078279a72805c468f6155784e78
emoore24 <evan.moore24@gmail.com>
2014-02-09 15:46:31 -0800

    Profile Page fixes
    + fix indentation in profile.styl, as well as css declaration
    + Move developer route above debug
    + Add l10n and utils dependencies
    + change page title on company load
    + Reference issue in todo in _macros/friends.html
    + Fix indentation, add target=“_blank”, and fix l10n translation in
    template

--- src/media/js/views/developer.js
+++ src/media/js/views/developer.js
@@ -1,10 +1,17 @@
-define('views/developer', [], function() {
+define('views/developer', ['l10n', 'utils'], function(l10n, utils) {
     return function(builder, args) {
         var dev_slug = args[0];
         builder.start('developers/profile.html', {dev_slug: dev_slug}).done(function() {
         });
 
+        var gettext = l10n.gettext;
+
         builder.z('type', 'leaf profile');
-        builder.z('title', gettext('Developer Profile'));
+        builder.z('title', gettext('Loading...'));
+
+        builder.onload('dev-search', function(company) {
+            builder.z('title', utils.translate(company.companyName));
+        });
+        
     };
 });
