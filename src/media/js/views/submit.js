21acf9f4d6ff1e9a795476cca8d60ae4b03c88ea
Tay Yang Shun <tay.yang.shun@gmail.com>
2014-02-18 10:27:48 +0800

    Game submit form allows multiple input for screenshots and videos url

--- src/media/js/views/submit.js
+++ src/media/js/views/submit.js
@@ -83,6 +83,36 @@ define('views/submit',
             // new dropzone('.submit-form', {
             //     uploadMultiple: true
             // });
+            var $fallback = $('.fallback');
+            function createInput($section) {
+                var $input = document.createElement('input');
+                $input.setAttribute('type', 'text');
+                $input.setAttribute('placeholder', $section.attr('data-placeholder'));
+                $section.append($input);
+            }
+            $.each($fallback, function() {
+                createInput($(this));
+            });
+            $('.screenshots, .videos').on('input', 'input[type=text]', function(e) {
+                var $input = $(e.target);
+                var $allInputs = $input.parent().children('input[type=text]');
+                var $emptyInputs = $allInputs.filter(function() {
+                    return $(this).val() === '';
+                });
+
+                if ($input.val() === '') {
+                    console.log($emptyInputs.length);
+                    for (var i = $emptyInputs.length - 1; i > 0; i--) {
+                        $emptyInputs[i].remove();
+                    }
+                } else {                    
+                    if ($emptyInputs.length === 0) {
+                        createInput($input.parent());
+                    }
+                }
+
+
+            });
         });
 
         builder.z('type', 'leaf submit');
