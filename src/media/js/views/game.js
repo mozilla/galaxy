e174dfd6cd0c06181d2d9af1daf87d2b734ab300
Tay Yang Shun <tay.yang.shun@gmail.com>
2014-03-01 06:03:51 +0800

    Feature games section done

--- src/media/js/views/game.js
+++ src/media/js/views/game.js
@@ -28,13 +28,44 @@ define('views/game',
 
     return function(builder, args) {
         var slug = args[0];
-    
-        builder.start('game/main.html', {slug: slug, page_url: utils.urlencode(window.location.href)});
+        var featured_games = [
+        {
+            title: 'Hex GL',
+            developer: 'Thibaut Despoulain',
+            icon: 'hexgl.png'
+        },
+        {
+            title: 'Batman: Arkham Origins',
+            developer: 'Armature Studio',
+            icon: 'batman.png'
+        },
+        {
+            title: 'Bastion',
+            developer: 'Supergiant Games',
+            icon: 'bastion.png'
+        },
+        {
+            title: 'Windborne',
+            developer: 'Hidden Path Entertainment',
+            icon: 'windborne.png'
+        },
+        {
+            title: 'Orion',
+            developer: 'Spiral Game Studios',
+            icon: 'orion.png'
+        },
+        {
+            title: 'Banished',
+            developer: 'Shining Rock Software',
+            icon: 'banished.png'
+        }
+        ];
+
+        builder.start('game/main.html', {slug: slug, page_url: utils.urlencode(window.location.href), featured_games: featured_games});
 
         builder.z('type', 'game');
         builder.z('title', gettext('Loading...'));
         builder.z('pagetitle', gettext('App Details'));
-
         builder.onload('game-data', function(game) {
             builder.z('title', utils.translate(game.name));
             twttr.widgets.load(); // Needed for loaded twitter widget scripts
