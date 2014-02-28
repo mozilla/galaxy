b7aa40803d8989c414f488e479195802c04132d5
scottmeng <blackridermkz@gmail.com>
2014-02-28 23:16:15 +0800

    add friend request button on user profile page

--- src/media/js/views/user.js
+++ src/media/js/views/user.js
@@ -1,4 +1,26 @@
-define('views/user', [], function() {
+define('views/user', ['z'], function(z) {
+
+    function requestFriend(user_id) {
+        requests.post(urls.api.url('user.friends.request'), {
+            recipient: user_id
+        }).done(function(data) {
+            notification.notification({message: gettext('Friend request sent')});
+            return console.log(data);
+        }).fail(function(data) {
+            notification.notification({message: gettext('Failed to submit friend request')});
+            return console.error(data);
+        });
+    }
+
+    z.body.on('click', '.add-friend', function() {
+        var $friend = $(this).closest('[data-user-id]');
+        var username = $friend.find('.profile-link').text();
+        var msg = gettext('Friend requested: {username}', {username: username});
+        notification.notification({message: msg});
+        requestFriend($friend.data('userId'));
+        $friend.remove();
+    });
+
     return function(builder, args) {
         var user_id = args[0];
         builder.start('users/profile.html', {user_id: user_id}).done(function() {
