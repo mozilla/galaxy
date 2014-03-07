7bd02ef3bf2d7e3674510f5fa9f0f1a5626e87c6
Charles Chong <CC.Charles.Chong@gmail.com>
2014-03-07 01:13:04 -0800

    added preliminary action to buttons

--- src/media/js/views/developer_dashboard.js
+++ src/media/js/views/developer_dashboard.js
@@ -1,4 +1,15 @@
-define('views/developer_dashboard', [], function() {
+define('views/developer_dashboard', ['z'], function(z) {
+
+	z.body.on('click', '.myGames-delete', function() {
+        var $this = $(this);
+        var $game = $this.closest('[data-game-slug]');
+        console.log('delete ' + $game.data('gameSlug'));
+    }).on('click', '.myGames-disable', function() {
+        var $this = $(this);
+        var $game = $this.closest('[data-game-slug]');
+        console.log('disable ' + $game.data('gameSlug'));
+    });
+
     return function(builder, args) {
         var user_id = args[0];
         builder.start('developers/developer-dashboard.html');
