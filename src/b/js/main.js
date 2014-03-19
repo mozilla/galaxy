39df0b5c1b4696b2457fa4b0470ecb53fafcc7fc
Christopher Van <cvan@mozilla.com>
2014-03-18 17:07:41 -0700

    hook up keyboard navigation for screenshot galleries

--- src/b/js/main.js
+++ src/b/js/main.js
@@ -2,7 +2,7 @@
 
     var $body = $(document.body);
 
-    $body.on('click', 'nav a', function () {
+    $body.on('click', 'nav a, .detail-game-thumbnail', function () {
         // It's worth noting that if you navigate with the keyboard arrows,
         // a click event is also synthesised.
         var $this = $(this);
@@ -10,11 +10,18 @@
         if ($this.hasClass('selected')) {
             return;
         }
-        $('nav a').removeClass('selected');
+        if ($this.filter('a').length) {
+            $('nav a.selected').removeClass('selected');
+        } else {
+            $this.closest('.detail-game-thumbnails').find('.selected').removeClass('selected');
+        }
         $this.addClass('selected');
     }).on('change', 'nav input', function () {
-        // window.history.pushState({}, '', '/game/' + $(this).val());
         window.location.hash = '#!/game/' + $(this).val();
+    }).on('click', '.detail-game-thumbnail', function () {
+        $('.detail-game-media-selected').css('background-image', $(this).css('background-image'));
+    }).on('change', '.detail-game-thumbnails input', function () {
+        $(this).siblings('.detail-game-thumbnail').click();
     });
 
     window.addEventListener('hashchange', function() {
@@ -40,6 +47,27 @@
         }
     }
 
-    $('nav').slinky();
+    window.addEventListener('keydown', function (e) {
+        switch (e.keyCode) {
+            case 39:  // right arrow key
+                $('nav a.selected input').trigger('blur');
+                var $group = $('.detail-game.selected .detail-game-thumbnails');
+                if ($group.find('input:checked').length) {
+                    console.log('right checked')
+                    //$group.find('input:checked').eq(0).trigger('focus').trigger('click').prop('checked', true);
+                    $group.find('input').eq(0).trigger('focus').trigger('click').prop('checked', true);
+                } else {
+                    $group.find('input').eq(0).trigger('focus').trigger('click').prop('checked', true);
+                }
+                e.preventDefault();
+                break;
+            case 38:  // up arrow key
+            case 40:  // down arrow key
+                break;
+            case 37:  // left arrow key
+                $('nav a.selected input').trigger('focus').trigger('click').prop('checked', true);
+                e.preventDefault();
+      }
+    }, false);
 
 })();
