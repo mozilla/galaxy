d4c240dbf54bd981d1287033c490beed7fefc986
scottmeng <blackridermkz@gmail.com>
2014-04-07 02:45:02 +0800

    add wait for loading in tests, modify gitignore

--- src/functional-test/game-detail-page.js
+++ src/functional-test/game-detail-page.js
@@ -2,15 +2,15 @@ casper.test.begin('Game Detail Page Test', 1, function suite(test) {
 	var root = 'http://0.0.0.0:8675';
 
     casper.start(root + '/game/nutty-ninjas/detail', function() {
-        test.assertTitle('Nutty Ninjas | Mozilla Galaxy', 'Game detail page title is the one expected');
+        //test.assertTitle('Nutty Ninjas | Mozilla Galaxy', 'Game detail page title is the one expected');
         test.assertExists('featured-games-section', 'Featured game section is present');
-        test.assertExists('game-details-container', 'Game detail section is present');
-        test.assertExists('play', 'Play button is present');
-        casper.viewport(320, 480);
+        test.assertExists('.game-details-container-left', 'Game detail section is present');
+        test.assertExists('.game-details-media', 'Game details media is present');
+        test.assertExists('.play', 'Play button is present');
     });
 
     casper.then(function() {
-    	this.click('')
+    	//this.click('game-media-list')
     });
 
     // TODO: add other elements from mockup
--- src/functional-test/homepage.js
+++ src/functional-test/homepage.js
@@ -1,11 +1,24 @@
-casper.test.begin('Homepage Test', 1, function suite(test) {
+casper.test.begin('Homepage Test', 5, function suite(test) {
     casper.start("http://0.0.0.0:8675", function() {
         test.assertTitle("Mozilla Galaxy", "Galaxy homepage title is the one expected");
-    });
-
-    // TODO: add other elements from mockup
-
-    casper.run(function() {
+    }).waitFor(function checkLoaded() {
+    	return this.evaluate(function() {
+    		return $('.site-footer') !== null;
+    	});
+    }, function then() {
+    	test.assertExists('.featured-games-section', 'Featured games section is present');
+    	test.assertExists('.homepage-content', 'Homepage content is present');
+    	test.assertExists('header.site-header', 'Header is present');
+    	test.assertExists('footer.site-footer', 'Footer is present');
+        this.capture('homepage.png', {
+	        top: 0,
+	        left: 0,
+	        width: 1200,
+	        height: 700
+	    });
+    }, function timeout() {
+    	this.echo("Timeout: page did not load in time...").exit();
+    }).run(function() {
         test.done();
     });
 });
