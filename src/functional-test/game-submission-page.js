9e92557de0ee3dbf4e3ae77c37080a4a427fe2c5
scottmeng <blackridermkz@gmail.com>
2014-04-19 13:43:27 +0800

    add game submission page tests

--- src/functional-test/game-submission-page.js
+++ src/functional-test/game-submission-page.js
@@ -1,23 +1,43 @@
-casper.test.begin('Game Submission Page Test', 5, function suite(test) {
+casper.test.begin('Game Submission Page Test', 3, function suite(test) {
 	var root = 'http://0.0.0.0:8675';
 
     casper.start(root + '/submit', function() {
         
     }).waitFor(function checkLoaded() {
-        return this.evaluate(function() {
-            // to do: check title text match
-            return $(document).find("title").text() === 'Nutty Ninjas | Mozilla Galaxy';
+        return this.evaluate(function() {   
+            return $(document).find("title").text() === 'Submit a Game | Mozilla Galaxy';
         });
     }, function then() {
-        // to do: add statis components assertions
+        test.assertExists('.game-form', 'Game form is present');
+        this.capture('game-submission-page.png', {
+            top: 0,
+            left: 0,
+            width: 1200,
+            height: 700
+        });
     }, function timeout() {
         this.echo("Timeout: page did not load in time...").exit();
     }).then(function() {
-        // to do: add user interactions tests
-        //      - clicking on "submit" button, assert notification
-        //      - enter empty space, new line, tab on required fields, assert style
-        //      - enter invalid url, assert style
-        //      - enter game title, assert game slug
+        // check if empty string can be accepted as game name
+        this.click('input[name=name]');
+        this.page.sendEvent('keypress', ' ');
+        this.wait(100, function() {
+            test.assert(this.evaluate(function() {
+                return $('.input[name=name]').css('border-color') === "rgb(218, 62, 90)";
+            }), 'empty name is not accepted');
+        });
+        
+        // check if game slug is correct representing game title
+        this.evaluate(function() {
+            $('input[name=name]').val('');
+        });
+        this.page.sendEvent('keypress', 'test/game with 123 and .,-<>?_*&4');
+        this.wait(100, function() {
+            test.assert(this.evaluate(function() {
+                return $('input[name=slug]').val() === 'testgame-with-123-and-4';
+            }), 'slug is correct');
+        });
+
         //      - enter url on media fields
         //      - upload image on media fields
         //      - enter valid game details and submit (will be later asserted on detail page)
