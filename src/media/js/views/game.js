f8121888830a4be70eadf70086f4b242373e831b
Birkan Uzun <birkanuzun27@gmail.com>
2014-05-12 02:55:10 -0400

    Add whitespace

--- src/media/js/views/game.js
+++ src/media/js/views/game.js
@@ -104,6 +104,7 @@ define('views/game',
             featured_games.attachScrollEvents($('.featured-games-section ul'));
             featured_games.attachScrollEvents($('.game-media-list'));
             $('.game-description').html(marked(game.description));
+            
         });
     };
 });
