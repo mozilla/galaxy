768492f5ee8f473f05193a0811f9bfddd99659df
Christopher Van <cvan@mozilla.com>
2014-07-09 00:49:09 -0700

    add WIP accelerometer-based controller

---
+++ controllers/mobile/controller.js
@@ -0,0 +1,7 @@
+(function () {
+
+// Open a connection to firebase.
+// TODO: Do not hardcode game slug and username.
+var answerRef = new Firebase('https://galaxy-controller.firebaseio.com/hexgl/cvan/');
+
+})();
