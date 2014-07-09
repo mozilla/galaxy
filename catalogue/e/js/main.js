3e381eb4111e8275472d588bfeb5f5a4ca104a4c
Christopher Van <cvan@mozilla.com>
2014-07-08 22:08:22 -0700

    add `galaxy.connect`, `galaxy.beginPlaying`, `galaxy.endPlaying` APIs; update catalogue with list of online/offline players

--- catalogue/e/js/main.js
+++ catalogue/e/js/main.js
@@ -1,2 +1,90 @@
 (function () {
+
+  var utils = {};
+  utils.escape = function (string) {
+    if (!string) {
+      return string;
+    }
+    return string.replace(/&/g, '&amp;')
+                 .replace(/</g, '&lt;')
+                 .replace(/>/g, '&gt;')
+                 .replace(/'/g, '&#39;')
+                 .replace(/"/g, '&#34;');
+  };
+
+  function updatePlayersLists() {
+    console.log('Updating the lists of players for each game');
+
+    var gamesPlayed = JSON.parse(localStorage.galaxy_gamesPlayed || '{}');
+    var gamesPlaying = JSON.parse(localStorage.galaxy_gamesPlaying || '{}');
+
+    $('.game[data-game]').each(function () {
+      var $this = $(this);
+      var slug = $this.data('game');
+
+      // TODO: We should normalise this in `localStorage`, but let's not change
+      // format until we know what we want to render on the UI.
+
+      var onlinePlayersObj = gamesPlaying[slug] || {};
+      var allPlayersObj = gamesPlayed[slug] || {};
+
+      var onlinePlayers = Object.keys(onlinePlayersObj);
+      var offlinePlayers = Object.keys(allPlayersObj).map(function (player) {
+        if (!(player in onlinePlayersObj)) {
+          return player;
+        }
+      }).filter(function (player) {
+        return typeof player !== 'undefined';
+      });
+
+      // Add the "Online" players list.
+      // TODO: Make this update in real time using WebSockets.
+      if (onlinePlayers.length) {
+        var playersHTML = '';
+        onlinePlayers.forEach(function (player) {
+          playersHTML += '<li>' + utils.escape(player) + '</li>\n';
+        });
+
+        var templateHTML = $('#online-players').html().replace('{players}', playersHTML);
+        if ($this.find('.details .online-players').length) {
+          // TODO: Instead of replacing the entire section, add/remove the
+          // necessary ones. (React is starting to sound like a good idea.)
+          console.log('replacing online');
+          $this.find('.details .online-players').replaceWith(templateHTML);
+        } else {
+          $this.find('.details').append(templateHTML);
+        }
+      } else if ($this.find('.details .online-players').length) {
+        // Again. This is a hack.
+        $this.find('.details .online-players').remove();
+      }
+
+      // Add the "Offline" players list.
+      // TODO: Make this update in real time using WebSockets.
+      if (offlinePlayers.length) {
+        var playersHTML = '';
+        offlinePlayers.forEach(function (player) {
+          playersHTML += '<li>' + utils.escape(player) + '</li>\n';
+        });
+
+        var templateHTML = $('#offline-players').html().replace('{players}', playersHTML);
+        if ($this.find('.details .offline-players').length) {
+          console.log('replacing offline');
+          $this.find('.details .offline-players').replaceWith(templateHTML);
+        } else {
+          $this.find('.details').append(templateHTML);
+        }
+      } else if ($this.find('.details .offline-players').length) {
+        $this.find('.details .offline-players').remove();
+      }
+    });
+  }
+
+  updatePlayersLists();
+
+  // Because the Page Visibility API is 💩, let's use `deviceorientation` for
+  // Chrome, `focus` for Firefox. Polyfill coming soon.
+  window.addEventListener('deviceorientation', updatePlayersLists);
+  window.addEventListener('focus', updatePlayersLists);
+
 })();
--- games/modal/include/include.js
+++ games/modal/include/include.js
@@ -2,9 +2,10 @@
 
 var $doc = $(document);
 var $body = $(document.body);
-var $main = $('main');
 
+var game = null;
 var utils = {};
+var user = null;
 
 utils.escape = function (string) {
   if (!string) {
@@ -26,14 +27,18 @@ utils.fieldFocused = function (string) {
 };
 
 
-function Modal(opts) {
-  // Create properties for `id`, `title`, and `content`.
+function Modal(opts, inject) {
+  // Create properties for `id`, `classes`, `title`, and `content`.
   Object.keys(opts).forEach(function (key) {
     this[key] = opts[key];
   }, this);
+
+  if (inject) {
+    this.inject();
+  }
 }
 
-Modal.closeAll = function () {
+Modal.closeAll = Modal.prototype.close = function () {
   // Close any open modal.
   $('.md-show').removeClass('md-show');
   $('.overlayed').removeClass('overlayed');
@@ -49,7 +54,7 @@ Modal.injectOverlay = function () {
 
 Modal.prototype.html = function () {
   return (
-    '<div class="md-modal md-effect-1" id="modal-' + this.id + '">' +
+    '<div class="md-modal md-effect-1 ' + (this.classes || '') + '" id="modal-' + this.id + '">' +
       '<div class="md-content">' +
         '<h3>' + utils.escape(this.title) + '</h3> ' +
         '<a class="md-close" title="Close"><span><div>Close</div></span></a>' +
@@ -63,10 +68,81 @@ Modal.prototype.inject = function () {
   Modal.injectOverlay();
 
   var $modal = $(this.html());
+
   $modal.appendTo($body);
   $body.addClass('overlayed');
-  return $modal[0];
+
+  return this.el = $modal[0];
 };
+
+Modal.prototype.open = function () {
+  this.el.classList.add('md-show');
+};
+
+
+function Game(opts) {
+  this.data = opts;
+}
+
+Game.prototype.get = function (key) {
+  console.log('Game.get');
+  return this.data[key];
+};
+
+Game.prototype.set = function (key, value) {
+  console.log('Game.set');
+  return this.data[key] = value;
+};
+
+
+function addToGamesPlayed() {
+  // TODO: Actually talk to API instead of keeped this local hashtable.
+  var gamesPlayed = JSON.parse(localStorage.galaxy_gamesPlayed || '{}');
+  if (!(game.get('name') in gamesPlayed)) {
+    gamesPlayed[game.get('name')] = {};
+  }
+
+  // Indicate that this user has played this game.
+  gamesPlayed[game.get('name')][user.get('username')] = true;
+
+  localStorage.galaxy_gamesPlayed = JSON.stringify(gamesPlayed);
+}
+
+function addToGamesPlaying() {
+  var gamesPlaying = JSON.parse(localStorage.galaxy_gamesPlaying || '{}');
+  if (!(game.get('name') in gamesPlaying)) {
+    gamesPlaying[game.get('name')] = {};
+  }
+
+  // Indicate that this user is playing this game.
+  gamesPlaying[game.get('name')][user.get('username')] = true;
+
+  localStorage.galaxy_gamesPlaying = JSON.stringify(gamesPlaying);
+}
+
+function removeFromGamesPlayed() {
+  var gamesPlayed = JSON.parse(localStorage.galaxy_gamesPlayed || '{}');
+  if (!(game.get('name') in gamesPlayed)) {
+    gamesPlayed[game.get('name')] = {};
+  }
+
+  // Indicate that this user has played this game.
+  delete gamesPlayed[game.get('name')][user.get('username')];
+
+  localStorage.galaxy_gamesPlayed = JSON.stringify(gamesPlayed);
+}
+
+function removeFromGamesPlaying() {
+  var gamesPlaying = JSON.parse(localStorage.galaxy_gamesPlaying || '{}');
+  if (!(game.get('name') in gamesPlaying)) {
+    gamesPlaying[game.get('name')] = {};
+  }
+
+  // Indicate that this user has played this game.
+  delete gamesPlaying[game.get('name')][user.get('username')];
+
+  localStorage.galaxy_gamesPlaying = JSON.stringify(gamesPlaying);
+}
 
 
 function User(opts) {
@@ -77,10 +153,40 @@ function User(opts) {
   this.authenticated = false;
 }
 
+User.prototype.get = function (key) {
+  console.log('User.get');
+  return this.data[key];
+};
+
+User.prototype.set = function (key, value) {
+  console.log('User.set');
+  return this.data[key] = value;
+};
+
 User.prototype.authenticate = function () {
   // TODO: Talk to API to make sure data is sane.
   console.log('User.authenticate');
   user.authenticated = true;
+};
+
+User.prototype.beginPlaying = function () {
+  console.log('User.beginPlaying');
+
+  // TODO: Actually begin tracking play time.
+  user.playing = true;
+
+  addToGamesPlaying();
+  addToGamesPlayed();
+};
+
+User.prototype.endPlaying = function () {
+  // TODO: Talk to API.
+  console.log('User.beginPlaying');
+
+  // TODO: Actually stop tracking play time.
+  user.playing = false;
+
+  removeFromGamesPlaying();
 };
 
 User.prototype.save = function () {
@@ -92,33 +198,81 @@ User.prototype.save = function () {
 var authForm = (
   '<form class="auth-form">' +
     '<label for="username">Username</label>' +
-    '<input type="text" id="username" name="username" placeholder="Choose a username">' +
+    '<input type="text" id="username" name="username" placeholder="Choose a username" autofocus>' +
   '</form>'
 );
 
 
-function authenticate() {
-  console.log('galaxy.authenticate');
+function connect(opts) {
+  return new Promise(function (resolve, reject) {
+    // TODO: Establish WebSocket connection to API server.
 
-  if (localStorage.galaxy_user) {
-    user = new User(JSON.parse(localStorage.galaxy_user));
-    user.authenticate();
-    return;
-  }
+    game = new Game(opts);
 
-  var authModal = new Modal({
-    id: 'auth',
-    title: 'Sign in',
-    content: authForm
-  }).inject();
-
-  setTimeout(function () {
-    // `setTimeout` for the fancy effect.
-    authModal.classList.add('md-show');
-  }, 150);
+    resolve();
+  });
 }
 
 
+function authenticate() {
+  return new Promise(function (resolve, reject) {
+    console.log('galaxy.authenticate');
+
+    if (localStorage.galaxy_user) {
+      user = new User(JSON.parse(localStorage.galaxy_user));
+      user.authenticate();
+
+      var modal = new Modal({
+        id: 'welcome',
+        classes: 'slim',
+        title: 'Welcome back',
+        content: 'Nice to see you again, ' + utils.escape(user.data.username)
+      }, true);
+
+      setTimeout(function () {
+        modal.open();
+      }, 150);
+
+      setTimeout(function () {
+        modal.close();
+        resolve();
+      }, 2000);
+
+      return;
+    }
+
+    var modal = new Modal({
+      id: 'auth',
+      classes: 'slim',
+      title: 'Sign in',
+      content: authForm
+    }, true);
+
+    setTimeout(function () {
+      // `setTimeout` for the fancy effect.
+      modal.open();
+    }, 150);
+
+    modal.el.addEventListener('submit', function (e) {
+      e.preventDefault();
+
+      // Create a new user and save locally.
+      user = new User({
+        username: e.target.elements[0].value,
+      });
+      user.authenticate();
+      user.save();
+
+      modal.close();
+
+      console.log('resolving');
+
+      resolve();
+    });
+  });
+}
+
+// Handle modal dismissals.
 $body.on('click', '.md-close, .md-overlay', function (e) {
   e.stopPropagation();
   Modal.closeAll();
@@ -129,21 +283,26 @@ $body.on('click', '.md-close, .md-overlay', function (e) {
   if (e.keyCode === 27) {
     Modal.closeAll();
   }
-}).on('submit', '.auth-form', function (e) {
-  e.preventDefault();
-
-  // Create a new user and save locally.
-  user = new User({
-    username: e.target.elements[0].value,
-  });
-  user.authenticate();
-  user.save();
 });
 
 
 if (!('game' in window)) {
   window.galaxy = {
-    authenticate: authenticate
+    authenticate: authenticate,
+    connect: connect,
+    beginPlaying: function () {
+      return user.beginPlaying();
+    },
+    endPlaying: function () {
+      return user.endPlaying();
+    },
+    getGame: function () {
+      return game;
+    },
+    getUser: function () {
+      return user;
+    },
+    utils: utils
   };
 }
 
